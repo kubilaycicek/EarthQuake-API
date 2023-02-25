fd48d1759e402b845d4941b31d63e3aa6f0d46c0
kubilaycicek <kubilaycicek0693@gmail.com>
2023-02-25 13:55:30 +0300

    Refactored EarthQaukeService

--- afad-api/src/main/java/com/kubilaycicek/afadapi/service/EarthQuakeService.java
+++ afad-api/src/main/java/com/kubilaycicek/afadapi/service/EarthQuakeService.java
@@ -2,7 +2,6 @@ package com.kubilaycicek.afadapi.service;
 
 import com.kubilaycicek.afadapi.payload.dto.EarthQuakeDto;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface EarthQuakeService {
--- afad-api/src/main/java/com/kubilaycicek/afadapi/service/EarthQuakeServiceImpl.java
+++ afad-api/src/main/java/com/kubilaycicek/afadapi/service/EarthQuakeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kubilaycicek.afadapi.service;
 
 import com.kubilaycicek.afadapi.payload.dto.EarthQuakeDto;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -12,31 +13,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class EarthQuakeServiceImpl implements EarthQuakeService {
     final String AFAD_WEB_URL = "https://deprem.afad.gov.tr/last-earthquakes.html";
 
     @Override
     public List<EarthQuakeDto> getEarthQuakeList() {
+
         try {
+
             Document doc = Jsoup.connect(AFAD_WEB_URL).get();
             Elements tableRows = doc.select("tr");
+
             List<EarthQuakeDto> quakeDtoList = new ArrayList<>();
-            for (Element row : tableRows.next()) {
-                EarthQuakeDto quakeDto = new EarthQuakeDto();
-                quakeDto.setDate(row.select("td").get(0).text());
-                quakeDto.setLatitude(row.select("td").get(1).text());
-                quakeDto.setLongtute(row.select("td").get(2).text());
-                quakeDto.setDepth(row.select("td").get(3).text());
-                quakeDto.setType(row.select("td").get(4).text());
-                quakeDto.setSize(row.select("td").get(5).text());
-                quakeDto.setLocation(row.select("td").get(6).text());
-                quakeDto.setEarthQuakeId(row.select("td").get(7).text());
-                quakeDtoList.add(quakeDto);
+
+            if (tableRows.size() != 0) {
+                tableRows.remove(0);
             }
+
+            tableRows.stream().forEach(item -> {
+                EarthQuakeDto quakeDto = new EarthQuakeDto();
+                quakeDto.setDate(item.select("td").get(0).text());
+                quakeDto.setLatitude(item.select("td").get(1).text());
+                quakeDto.setLongtute(item.select("td").get(2).text());
+                quakeDto.setDepth(item.select("td").get(3).text());
+                quakeDto.setType(item.select("td").get(4).text());
+                quakeDto.setSize(item.select("td").get(5).text());
+                quakeDto.setLocation(item.select("td").get(6).text());
+                quakeDto.setEarthQuakeId(item.select("td").get(7).text());
+                quakeDtoList.add(quakeDto);
+
+            });
 
             return quakeDtoList;
         } catch (Exception exception) {
-            System.out.println("Sorry");
+            log.error("EarthQuake Service : " + exception.getMessage());
         }
         return new ArrayList<>();
     }
