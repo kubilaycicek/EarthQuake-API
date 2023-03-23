c40e462814ce88d7159ad94a777be671055bc168
kubilaycicek <kubilaycicek0693@gmail.com>
2023-03-23 23:13:33 +0300

    OpenAPI Annotations

--- afad-api/src/main/java/com/kubilaycicek/afadapi/controller/EarthquakeController.java
+++ afad-api/src/main/java/com/kubilaycicek/afadapi/controller/EarthquakeController.java
@@ -1,19 +1,16 @@
 package com.kubilaycicek.afadapi.controller;
 
-import com.kubilaycicek.afadapi.payload.dto.EarthQuakeDto;
 import com.kubilaycicek.afadapi.payload.request.BetweenMinAndMaxSizeRequest;
 import com.kubilaycicek.afadapi.payload.response.GetEarthQuakeListResponse;
 import com.kubilaycicek.afadapi.service.EarthQuakeService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -23,21 +20,41 @@ public class EarthquakeController {
 
     private final EarthQuakeService earthQuakeService;
 
+    @Operation(
+            tags = "EarthQuake Controller",
+            summary = "All Records",
+            description = "Get All Records"
+    )
     @GetMapping("list")
     public ResponseEntity<GetEarthQuakeListResponse> getEarthQuakeList() {
         return ResponseEntity.status(HttpStatus.OK).body(new GetEarthQuakeListResponse(earthQuakeService.getEarthQuakeList()));
     }
 
+    @Operation(
+            tags = "EarthQuake Controller",
+            summary = "Get List By City",
+            description = "Get List Filter Records By City"
+    )
     @GetMapping("list/search/city/name/{name}")
     public ResponseEntity<GetEarthQuakeListResponse> getEarthQuakeByCityName(@PathVariable String name) {
         return ResponseEntity.status(HttpStatus.OK).body(new GetEarthQuakeListResponse(earthQuakeService.getListByCityName(name)));
     }
 
+    @Operation(
+            tags = "EarthQuake Controller",
+            summary = "Filter List Between Min And Max Size",
+            description = "Get List Filter Records Betweeb Min And Max Size"
+    )
     @GetMapping("list/between-min-and-max-size")
     public ResponseEntity<GetEarthQuakeListResponse> getListFilterBetweenMinAndMaxSize(@RequestBody BetweenMinAndMaxSizeRequest request) {
         return ResponseEntity.status(HttpStatus.OK).body(new GetEarthQuakeListResponse(earthQuakeService.getListBetweenMinSizeAndMaxSize(request.getMin(), request.getMax())));
     }
 
+    @Operation(
+            tags = "EarthQuake Controller",
+            summary = "Clear Cache",
+            description = "Clear Cache endpoint"
+    )
     @Scheduled(fixedRateString = "${cache.list.earthQuakeListTTL}")
     @CacheEvict("earthQuakeList")
     @PostMapping("clear-memory")
