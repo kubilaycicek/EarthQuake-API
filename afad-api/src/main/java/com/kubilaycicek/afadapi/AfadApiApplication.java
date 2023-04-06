e9135f8ed31ba920979e5a279ff280b0b13492e8
kubilaycicek <kubilaycicek0693@gmail.com>
2023-04-07 01:31:25 +0300

    removed some package imports

--- afad-api/src/main/java/com/kubilaycicek/afadapi/AfadApiApplication.java
+++ afad-api/src/main/java/com/kubilaycicek/afadapi/AfadApiApplication.java
@@ -1,15 +1,8 @@
 package com.kubilaycicek.afadapi;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-
-import java.io.IOException;
 
 @EnableCaching
 @SpringBootApplication
