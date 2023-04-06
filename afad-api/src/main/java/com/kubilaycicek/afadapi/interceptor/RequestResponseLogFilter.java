ff063c2642b3745c1bd09924c0522f3a0655bef7
kubilaycicek <kubilaycicek0693@gmail.com>
2023-04-07 00:40:08 +0300

    Implemented Request Response Log Filter

---
+++ afad-api/src/main/java/com/kubilaycicek/afadapi/interceptor/RequestResponseLogFilter.java
@@ -0,0 +1,52 @@
+package com.kubilaycicek.afadapi.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+@Slf4j
+@Component
+public class RequestResponseLogFilter extends OncePerRequestFilter {
+
+    private String getStringValue(byte[] contentAsByteArray, String characterEncoding) {
+        try {
+            return new String(contentAsByteArray, 0, contentAsByteArray.length, characterEncoding);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(requestWrapper, responseWrapper);
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+        String requestBody = getStringValue(requestWrapper.getContentAsByteArray(),
+                request.getCharacterEncoding());
+        String responseBody = getStringValue(responseWrapper.getContentAsByteArray(),
+                response.getCharacterEncoding());
+
+        log.info(
+                "FINISHED PROCESSING : METHOD={}; REQUESTURI={}; REQUEST PAYLOAD={}; RESPONSE CODE={}; RESPONSE={}; TIM TAKEN={}",
+                request.getMethod(), request.getRequestURI(), requestBody, response.getStatus(), responseBody,
+                timeTaken);
+        responseWrapper.copyBodyToResponse();
+    }
+
+}
